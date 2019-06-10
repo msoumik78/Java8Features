d5b1358a8dd47c5a32391d61c7fb54bded7fcbe1
Soumik Mukherjee <msoumik78@gmail.com>
2019-06-10 21:42:26 +0530

    Added 4 functional interfaces/lambdas to the project

--- src/Java8LambdasDemo.java
+++ src/Java8LambdasDemo.java
@@ -1,14 +1,19 @@
 
 public class Java8LambdasDemo {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		
-		Java8FunctionalInterface java8Lambdas = (n) -> Math.sqrt(n);
-		System.out.println("SquareRoot of 16 is : "+java8Lambdas.findSquareRoot(16));
-		System.out.println("SquareRoot of 9 is : "+java8Lambdas.findSquareRoot(9));		
+		Java8FunctionalInterface1 java8Lambda1 = () -> System.out.println("Greeting from FunctionalInterace1");
+		java8Lambda1.showStaticMessage();
+		
+		Java8FunctionalInterface2 java8Lambda2 = (String msg) -> System.out.println("Greeting from FunctionalInterace2 :"+msg);
+		java8Lambda2.showDynamicMessage("Hi from main method");
+		
+		Java8FunctionalInterface3 java8Lambda3 = (int number) ->Math.sqrt(number);
+		System.out.println("Output from FunctionalInterace3 : Square root of 16 = "+java8Lambda3.findSquareRoot(16));
+		
+		Java8FunctionalInterface4 java8Lambda4 = (int number1, int number2) -> number1 * number2;
+		System.out.println("Output from FunctionalInterace4 : Multiply 4 and 6 = "+java8Lambda4.multiply(4,6));
 
 	}
 
