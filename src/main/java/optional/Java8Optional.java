d6840b982224809bce351004ef23e52e8c9f3ad1
msoumik78 <msoumik78@gmail.com>
2020-01-12 23:20:24 +0530

    Update Java8Optional.java

--- src/main/java/optional/Java8Optional.java
+++ src/main/java/optional/Java8Optional.java
@@ -5,18 +5,17 @@ import java.util.Optional;
 public class Java8Optional {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	     Optional<String> language = Optional.of("Java");
+	     Optional<String> javaLanguage = Optional.of("Java");
 	        Optional<String> emptyLanguage= Optional.empty();
 
-	        if (language.isPresent()) {
-	            System.out.println("Value available and value is :"+language.get());
+	        if (javaLanguage.isPresent()) {
+	            System.out.println("Value available and value is :"+javaLanguage.get());
 	        } else {
 	            System.out.println("Value not available.");
 	        }
 
-	        language.ifPresent(g -> System.out.println("In language Option, value available."));
+	        javaLanguage.ifPresent(g -> System.out.println("In language Option, value available."));
 
 	        emptyLanguage.ifPresent(g -> System.out.println("In emptyLanguage Option, value available."));		
 		
